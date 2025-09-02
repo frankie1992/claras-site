45e34d194a8fd8da83a9c726b86b95f50ca55a98
Francisco <francisco.miranda.92@gmail.com>
2025-09-01 21:14:54 -0500

    fix error

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -8,8 +8,8 @@ export function isValidEmail(email: string) {
 export function remove_hash_from_url() {
     try {
         if ("history" in window) { // Check if History API is supported by the browser
-  history.pushState({}, null, window.location.href.split('#')[0]);
-}
+            history.pushState({}, '', window.location.href.split('#')[0]);
+        }
 
     } catch (error) {
         console.error(error)
